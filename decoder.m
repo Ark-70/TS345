2f819c0895dd1afc5c2e7453e43f31f34c836463
TRAPARIC David <david.traparic@enseirb-matmeca.fr>
2020-11-04 08:18:39 +0100

    save avant confinement

--- decoder.m
+++ decoder.m
@@ -9,8 +9,8 @@ matrix_V2C = matrix_C2V;
 [y, x] = find(H);
 
 %% première passe avec tout C2V à 0 donc ca laisse passer que les LLRs
-nb_noeudsvar = size(H, 2);
-Lc_matrix = reshape(Lc, nb_noeudsvar, length(Lc)/nb_noeudsvar)';
+nb_noeudsparite = size(H, 2);
+Lc_matrix = reshape(Lc, nb_noeudsparite, length(Lc)/nb_noeudsparite)';
 
 %% Je veux mettre tous les noeuds de parités (carrés) dans les noeuds de var (ronds)
 % sachant qu'on comprend tous les n_parités = 0, + sauf ceux qu'on rajoute
@@ -23,13 +23,21 @@ Lc_matrix = reshape(Lc, nb_noeudsvar, length(Lc)/nb_noeudsvar)';
 % On a des msg de 3, codés avec redondance en mot de code de 6
 
 % premier mot de code
-premier_mdc = Lc(:, 1);
+premier_mdc = Lc_matrix(1, :);
 
-find(H:, premier_mdc(1));
+% On veut dupliquer le premier bit du mot de code dans tous les noeuds de
+% parité correspondant
 
-for i=1:length(x)
-    reshape()
-%     matrix_V2C(y, x) = 
+% Après on fait des +(XOR) pour avoir la valeur des noeuds de variables
+% find(H:, premier_mdc(1));
 
+%% Init = V2C=LLR||0
+% Itération = 1
 
-end  % function
+LLR_repetes = repmat(premier_mdc, 3, 1);
+matrix_V2C = H.*LLR_repetes;
+
+matrix_C2V = 
+
+for i = 1:
+2*atanh*(tanh(LC1/2)*tanh(LC2/2)*tanh(LC3/2))
